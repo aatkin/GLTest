b641f12210a5333cd740b766182b859ebdf9ff4b
Anssi Kinnunen <aatkin@utu.fi>
2014-06-10 11:40:08 +0300

    dragon commit

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,9 @@ GLFWwindow *create_glfw()
 
     glfwSetErrorCallback(errorCallback);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+//    glfwWindowHint(GLFW_SAMPLES, 16);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
