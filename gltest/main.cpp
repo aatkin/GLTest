42e87486731e59f3a211bed3ed11cdc436b3189b
Anssi Kinnunen <aatkin@utu.fi>
2014-06-13 18:08:42 +0300

    ported x11, added PI-definition, otherwise the same

--- gltest/main.cpp
+++ gltest/main.cpp
@@ -2,74 +2,74 @@
 
 namespace GLTest
 {
-    /**
-     *  Error-Callback function for GLFW.
-     */
-    void errorCallback(int error, const char *description)
+/**
+ *  Error-Callback function for GLFW.
+ */
+void errorCallback(int error, const char *description)
+{
+    std::cerr << description << " : " << error << std::endl;
+}
+
+/**
+ * Create a GLFW-controlled window that OpenGL can use, and return a pointer to it.
+ */
+GLFWwindow *create_glfw()
+{
+    glfwInit();
+
+    glfwSetErrorCallback(errorCallback);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(960, 540, "Hello wurld", nullptr, nullptr); // Windowed
+
+    return window;
+}
+
+GLuint loadShaderFromFile( std::string path, GLenum shaderType )
+{
+    //Open file
+    GLuint shaderID = 0;
+    std::string shaderString;
+    std::ifstream sourceFile( path.c_str() );
+
+    //Source file loaded
+    if( sourceFile )
     {
-        std::cerr << description << " : " << error << std::endl;
-    }
+        //Get shader source
+        shaderString.assign( ( std::istreambuf_iterator< char >( sourceFile ) ), std::istreambuf_iterator< char >() );
+        //Create shader ID
+        shaderID = glCreateShader( shaderType );
 
-    /**
-     * Create a GLFW-controlled window that OpenGL can use, and return a pointer to it.
-     */
-    GLFWwindow *create_glfw()
-    {
-        glfwInit();
+        //Set shader source
+        const GLchar* shaderSource = shaderString.c_str();
+        glShaderSource( shaderID, 1, (const GLchar**)&shaderSource, NULL );
 
-        glfwSetErrorCallback(errorCallback);
+        //Compile shader source
+        glCompileShader( shaderID );
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-
-        GLFWwindow* window = glfwCreateWindow(960, 540, "Hello wurld", nullptr, nullptr); // Windowed
-
-        return window;
-    }
-
-    GLuint loadShaderFromFile( std::string path, GLenum shaderType )
-    {
-        //Open file
-        GLuint shaderID = 0;
-        std::string shaderString;
-        std::ifstream sourceFile( path.c_str() );
-
-        //Source file loaded
-        if( sourceFile )
+        //Check shader for errors
+        GLint shaderCompiled = GL_FALSE;
+        glGetShaderiv( shaderID, GL_COMPILE_STATUS, &shaderCompiled );
+        if( shaderCompiled != GL_TRUE )
         {
-            //Get shader source
-            shaderString.assign( ( std::istreambuf_iterator< char >( sourceFile ) ), std::istreambuf_iterator< char >() );
-            //Create shader ID
-            shaderID = glCreateShader( shaderType );
-
-            //Set shader source
-            const GLchar* shaderSource = shaderString.c_str();
-            glShaderSource( shaderID, 1, (const GLchar**)&shaderSource, NULL );
-
-            //Compile shader source
-            glCompileShader( shaderID );
-
-            //Check shader for errors
-            GLint shaderCompiled = GL_FALSE;
-            glGetShaderiv( shaderID, GL_COMPILE_STATUS, &shaderCompiled );
-            if( shaderCompiled != GL_TRUE )
-            {
-                std::cout << "Unable to compile shader: " << shaderID << ", source: " << shaderSource << std::endl;
-                glDeleteShader( shaderID );
-                shaderID = 0;
-            }
+            std::cout << "Unable to compile shader: " << shaderID << ", source: " << shaderSource << std::endl;
+            glDeleteShader( shaderID );
+            shaderID = 0;
         }
-        else
-        {
-            printf( "Unable to open file %s\n", path.c_str() );
-        }
-
-        return shaderID;
     }
+    else
+    {
+        printf( "Unable to open file %s\n", path.c_str() );
+    }
+
+    return shaderID;
+}
 }
 
 int main()
@@ -83,7 +83,6 @@ int main()
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
-//    glEnable(GL_STENCIL_TEST);
 
     std::cout << "OpenGL version " << glGetString(GL_VERSION) << std::endl;
 
@@ -169,21 +168,20 @@ int main()
     /** Projection matrix uniform setup */
     glm::mat4 projection = glm::perspective(45.0f, 16.0f / 9.0f, 0.1f, 100.0f);
     glm::mat4 view = glm::lookAt(
-        glm::vec3(0.5, 1.0, 1.5), // Camera is at (0, 0, 5), in World Space
-        glm::vec3(0, 0, 0), // and looks at the origin
-        glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
+                         glm::vec3(0.0f, 1.5f, 3.5f), // "Camera" position
+                         glm::vec3(0, 0, 0), // "Camera" look-at vector
+                         glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+                     );
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 MVP = projection * view * model;
 
     GLuint matrixID = glGetUniformLocation(shaderProgram, "MVP");
     glUniformMatrix4fv(matrixID, 1, GL_FALSE, &MVP[0][0]);
 
-    clock_t start_t, end_t;
-//    int total;
-//    std::string str;
-    double delta;
-    double rotate_factor = 0.10f;
+    double start, stop, delta, rotation, elapsed_time = 0.0f;
+    double x = 0.0f, y = 0.0f, move_speed = 0.0f;
+    glm::mat4 rotate_m4, translate_m4, mult_m4;
+    int frames = 0;
 
     /** Main loop of the program. */
     while(!glfwWindowShouldClose(window))
@@ -191,38 +189,54 @@ int main()
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, GL_TRUE);
 
-        start_t = clock();
+        start = glfwGetTime();
 
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//        glClear(GL_COLOR_BUFFER_BIT);
 
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
         glBindBuffer(GL_ARRAY_BUFFER, vbo_color);
         glVertexAttribPointer(colorAttrib, 4, GL_FLOAT, GL_FALSE, 0, 0);
-
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 
         glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
-//        glFinish();
-
+        glFinish();
 
         glfwSwapBuffers(window);
-//        glfwWaitEvents();
+
+        /** Get elapsed time in seconds */
+        stop = glfwGetTime();
+        delta = stop - start;
+        /** Rotate a full revolution once every second */
+        rotation = (2.0f * PI * delta) * (180.0f / PI);
+
+        /** Circle around at the current draw speed */
+        x = glm::cos(move_speed);
+        y = glm::sin(move_speed);
+
         glfwPollEvents();
 
-        end_t = clock();
-        delta = (double)(end_t - start_t) * rotate_factor;
-//        model = glm::mat4(1.0f);
-        model = glm::rotate(model, (glm::mediump_float)delta, glm::vec3(0.25f, 1.0f, 0.5f));
-        MVP = projection * view * model;
-        glUniformMatrix4fv(matrixID, 1, GL_FALSE, &MVP[0][0]);
-//        total = (int)(end_t - start_t);
-//        str = std::to_string(total) + "ms";
-//        std::cout << str << std::endl;
-//        glfwSetWindowTitle(window, str.c_str());
+        translate_m4 = glm::translate(glm::mat4(1.0f), glm::vec3(x * 1.5f, y / 2.0f, 0.0f));
+        rotate_m4 = glm::rotate(rotate_m4, (glm::mediump_float)(rotation / 3.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        mult_m4 = translate_m4 * rotate_m4;
+
+        MVP = projection * view * model * mult_m4;
+        glUniformMatrix4fv(matrixID, 1, GL_FALSE, glm::value_ptr(MVP));
+
+        move_speed += delta;
+
+        if(elapsed_time >= 1.0f)
+        {
+            std::string title = "Hello wurld - ";
+            title.append(std::to_string((elapsed_time * 1000.0f) / frames));
+            title.append("ms");
+            glfwSetWindowTitle(window, title.c_str());
+            elapsed_time = 0.0f;
+            frames = 0;
+        }
+        elapsed_time += delta;
+        frames = frames + 1;
     }
 
     glfwTerminate();
@@ -231,6 +245,7 @@ int main()
     glDeleteShader(vertexShader);
 
     glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
 
     glDeleteVertexArrays(1, &vao_triangle);
     return 0;
--- gltest/main.hpp
+++ gltest/main.hpp
@@ -1,19 +1,22 @@
 #ifndef MAIN_HPP_INCLUDED
 #define MAIN_HPP_INCLUDED
 
-#include <ctime>
 #include <iostream>
 #include <fstream>
 #include <string>
 
+#define PI 3.141592653589793238463
+
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GL/glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void errorCallback(int error, const char *description);
-GLFWwindow* create_glfw();
+GLFWwindow *create_glfw();
 GLuint loadShaderFromFile( std::string path, GLenum shaderType );
 #endif // MAIN_HPP_INCLUDED
